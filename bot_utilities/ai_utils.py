e1a04e8ccfd1b43ab1f07a19b302f3ade5815f28
MrCargon <xerleon1951@gmail.com>
2023-10-03 20:27:40 -0700

    _update

--- bot_utilities/ai_utils.py
+++ bot_utilities/ai_utils.py
@@ -26,14 +26,12 @@ from langchain.prompts import ChatPromptTemplate
 import requests
 
 
-load_dotenv(find_dotenv())
-openai.api_key = os.getenv("OPENAI_API_KEY")
 load_dotenv()
 current_language = load_current_language()
 internet_access = config['INTERNET_ACCESS']
 
-# openai.api_key = os.getenv('CHIMERA_GPT_KEY')
-# openai.api_base = "https://api.naga.ac/v1"
+openai.api_key = os.getenv('CHIMERA_GPT_KEY')
+openai.api_base = "https://api.naga.ac/v1"
 def sdxl(prompt):
     response = openai.Image.create(
     model="sdxl",
@@ -43,88 +41,19 @@ def sdxl(prompt):
 )
     return response['data'][0]["url"]
 
-def knowledge_retrieval(query):    
-    # Define the data to be sent in the request
-    data = {
-        "params":{
-            "query":query
-        },
-        "project": "7275c675da5d-4296-93a7-346bdf65dbd8"
-    }
 
-    # Convert Python object to JSON string
-    data_json = json.dumps(data)
+async def _mentioned_user_func(user, message):
+  member = user.guild.get_member(user.id)
+  if member:
+    status = member.status
+    response = f"{message} and member display name is : {member.display_name} Current Status Is: {status}"
+  else:
+    response = f"{message} and member display name is : {member.display_name} Current Status Is: That user is not in the server."
+  
+  return response
+  
 
-    # Send the POST request
-    response = requests.post("https://api-bcbe5a.stack.tryrelevance.com/latest/studios/94942743-df19-41a4-8e7c-64688d12bb44/trigger_limited", data=data_json)
-    
-    # Check the response status code
-    if response.status_code == 200:
-        return response.json()["output"]["answer"]
-    else:
-        print(f"HTTP request failed with status code {response.status_code}") 
-
-def summary(content):
-    llm = ChatOpenAI(temperature = 0, model = "gpt-3.5-turbo-16k-0613")
-    text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"], chunk_size = 10000, chunk_overlap=500)
-    docs = text_splitter.create_documents([content])
-    map_prompt = """
-    Write a summary of the following text:
-    "{text}"
-    SUMMARY:
-    """
-    map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
-    
-    summary_chain = load_summarize_chain(
-        llm=llm, 
-        chain_type='map_reduce',
-        map_prompt = map_prompt_template,
-        combine_prompt = map_prompt_template,
-        verbose = True
-    )
-
-    output = summary_chain.run(input_documents=docs,)
-
-    return output
-
-
-def scrape_website(url: str):
-    #scrape website, and also will summarize the content based on objective if the content is too large
-    #objective is the original objective & task that user give to the agent, url is the url of the website to be scraped
-
-    print("Scraping website...")
-    # Define the headers for the request
-    headers = {
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json',
-    }
-
-    # Define the data to be sent in the request
-    data = {
-        "url": url        
-    }
-
-    # Convert Python object to JSON string
-    data_json = json.dumps(data)
-
-    # Send the POST request
-    response = requests.post("https://chrome.browserless.io/content?token=2db344e9-a08a-4179-8f48-195a2f7ea6ee", headers=headers, data=data_json)
-    
-    # Check the response status code
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        text = soup.get_text()
-        print("CONTENTTTTTT:", text)
-        if len(text) > 10000:
-            output = summary(text)
-            return output
-        else:
-            return text
-    else:
-        print(f"HTTP request failed with status code {response.status_code}")     
-
-
-def search(query):
+async def search(prompt):
     """
     Asynchronously searches for a prompt and returns the search results as a blob.
 
@@ -137,159 +66,41 @@ def search(query):
     Raises:
         None
     """
+    if not internet_access or len(prompt) > 200:
+        return
+    search_results_limit = config['MAX_SEARCH_RESULTS']
 
-    endpoint = "https://ddg-api.herokuapp.com/search"
-    params = {
-        'query': query,  # Replace with your search query
-        'limit': 5  # Replace with your desired limit
-    }
-    
-    # Make the GET request
-    response = requests.get(endpoint, params=params)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        results = response.json()
-        return results
+    url_match = re.search(r'(https?://\S+)', prompt)
+    if url_match:
+        search_query = url_match.group(0)
     else:
-        return (f"Didn't get any results")
+        search_query = prompt
 
+    if search_query is not None and len(search_query) > 200:
+        return
 
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    blob = f"Search results for: '{search_query}' at {current_time}:\n"
+    if search_query is not None:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://ddg-api.awam.repl.co/api/search',
+                                       params={'query': search_query, 'maxNumResults': search_results_limit}) as response:
+                    search = await response.json()
+        except aiohttp.ClientError as e:
+            print(f"An error occurred during the search request: {e}")
+            return
 
-def research(query):
-    system_message = SystemMessage(
-        content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
-            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
-            
-            Please make sure you complete the objective above with the following rules:
-            1/ You will always searching for internal knowledge base first to see if there are any relevant information
-            2/ If the internal knowledge doesnt have good result, then you can go search online
-            3/ While search online:
-                a/ You will try to collect as many useful details as possible
-                b/ If there are url of relevant links & articles, you will scrape it to gather more information
-                c/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
-    )
-
-    agent_kwargs = {
-        "system_message": system_message,
-    }
-
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
-    llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
-    tools = [        
-        Tool(
-            name="Knowledge_retrieval",
-            func=knowledge_retrieval,
-            description="Use this to get our internal knowledge base data for curated information, always use this first before searching online"
-        ),      
-        Tool(
-            name = "Google_search",
-            func = search,
-            description = "Always use this to answer questions about current events, data, or terms that you don't really understand. You should ask targeted questions"
-        ),          
-        Tool(
-            name = "Scrape_website",
-            func = scrape_website,
-            description = "Use this to load content from a website url"
-        ),   
-    ]
-
-    agent = initialize_agent(
-        tools, 
-        llm, 
-        agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
-        verbose=False,
-        agent_kwargs=agent_kwargs,
-    )
-
-    results = agent.run(query)
-
-    return results
-
-
-def trigger_github_weekly_trending_repo_scrape():
-    url = "https://api.browse.ai/v2/robots/0c0f94bf-207a-4660-8ade-238cd778bb25/tasks"
-
-    payload = {"inputParameters": 
-               {"originUrl": "https://github.com/trending"}
-            }
-    headers = {"Authorization": "Bearer ec2cc08b-3343-47c9-9dd3-dc5d40d4aa3b:dead067b-d485-496d-a3e0-4902339f6cfe"}
-
-    response = requests.request("POST", url, json=payload, headers=headers)
-
-    print("id: ", response.json()["result"]["id"], "is :", response.text)
-    return response.json()["result"]["id"]
-
-def retrieve_github_weekly_trending_repo(task_id):
-    url = f"https://api.browse.ai/v2/robots/0c0f94bf-207a-4660-8ade-238cd778bb25/tasks/{task_id}"
-
-    headers = {"Authorization": "Bearer ec2cc08b-3343-47c9-9dd3-dc5d40d4aa3b:dead067b-d485-496d-a3e0-4902339f6cfe"}
-
-    response = requests.request("GET", url, headers=headers)
-
-    return response.json()
-
-def get_github_weekly_trending_repo():
-    task_id = trigger_github_weekly_trending_repo_scrape()    
-
-    while True:
-        time.sleep(5)
-
-        response = retrieve_github_weekly_trending_repo(task_id)
-
-        # print(response)
-        if response["statusCode"] == 200:
-            if response["result"]["status"] == "successful":
-                repos = response["result"]["capturedLists"]
-                return repos                                 
-            elif response["result"]["status"] == "failed":
-                return "failed to get data"
-        elif response["statusCode"] in {400, 401, 403, 404, 500, 503}:
-            return response["messageCode"]
-
-def filter_ai_github_repos(repos):
-    model = ChatOpenAI()
-
-    prompt_template = """
-    {repos} 
-    Above is the list of scraped trending github repos this week, 
-    can you help me filter out ones that is related to AI, knowledge graph, computer vision, large language model?
-
-    The report should be in certain format:
-    "🚀 Daily trending AI projects:
-
-    **coqui-ai / TTS**
-    - 🌟 3,952 stars this week | 18,952 total stars
-    - 📖 a deep learning toolkit for Text-to-Speech, battle-tested in research and production
-    - 🌐 https://github.com/coqui-ai/TTS
-
-    **tldraw / tldraw**
-    - 🌟 2,196 stars this week | 20,812 total stars
-    - 📖 a very good whiteboard
-    - 🌐 https://github.com/yoheinakajima/instagraph
-
-    ...."
-
-    if there is no any relevant repo, you can just say "Looks like no new interesting AI project today, let me know if I missed any pls!"
-    """
-    prompt = ChatPromptTemplate.from_template(prompt_template)
-
-    chain = prompt | model
-
-    results = chain.invoke({"repos": repos})
-
-    return results.content
-
-def generate_trending_git_report():
-    repos = get_github_weekly_trending_repo()
-
-    filtered_repos = filter_ai_github_repos(repos)
-
-    return filtered_repos
-
+        for index, result in enumerate(search):
+            try:
+                blob += f'[{index}] "{result["Snippet"]}"\n\nURL: {result["Link"]}\n'
+            except Exception as e:
+                blob += f'Search error: {e}\n'
+            blob += "\nSearch results allows you to have real-time information and the ability to browse the internet\n.As the links were generated by the system rather than the user, please send a response along with the link if necessary.\n"
+        return blob
+    else:
+        blob = "No search query is needed for a response"
+    return blob
     
 async def fetch_models():
     return openai.Model.list()
@@ -301,12 +112,9 @@ def create_agent(id, user_name, ai_name, instructions):
         content=instructions
     )
 
-    # Convert system message to string
-    system_message_str = str(system_message)
-
     agent_kwargs = {
         "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
-        "system_message": system_message_str,
+        "system_message": system_message,
     }
 
     memory = ConversationBufferWindowMemory(memory_key="memory", return_messages=True, ai_prefix=ai_name, user_prefix=user_name)
@@ -315,14 +123,14 @@ def create_agent(id, user_name, ai_name, instructions):
     tools = [                     
         Tool(
             name = "research",
-            func = research,
+            func = search,
             description = "Always use this to answer questions about current events, data, or terms that you don't really understand. You should ask targeted questions"
         ),           
-        Tool(
-            name = "Scrape_website",
-            func = scrape_website,
-            description = "Use this to load content from a website url"
-        ),   
+        #Tool(
+        #    name = "Scrape_website",
+        #    func = scrape_website,
+        #    description = "Use this to load content from a website url"
+        #),   
     ]    
 
     agent = initialize_agent(
@@ -337,7 +145,6 @@ def create_agent(id, user_name, ai_name, instructions):
     agents[id] = agent
     
     return agent
-
 
 def generate_response(instructions, user_input):   
     id = user_input["id"]    
@@ -372,7 +179,6 @@ def generate_response_old(instructions, search, history):
     )
     message = response.choices[0].message.content
     return message
-
 
 def generate_gpt4_response(prompt):
     messages = [
